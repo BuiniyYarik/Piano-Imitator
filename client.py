0ae7f88c10b57c23ece663ab6f0ca4e492a9ff8c
Yaroslav Sokolov <sokoslav1707@gmail.com>
2022-07-08 12:37:05 +0400

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,13 @@
-# TCP CLIENT 
 import socket
 
+# create client socket for Wi-Fi network of ESP32 
 host = '192.168.4.1'
 port = 9900
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect((host, port))
 
 try:
+    # receive and print data from server while connection is opened
     while True:
         data = sock.recv(64).decode('utf-8')
         if data == "Close session":
@@ -19,6 +19,7 @@ except socket.error as e:
 except Exception as e:
     print(e)
 finally:
+    # close client socket
     print("Closing connection to the server...")
     print("See you again!")
     sock.close()
