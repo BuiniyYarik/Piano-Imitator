9def53849e9814b228c8a3f3c83014ed03d6961a
Yaroslav Sokolov <sokoslav1707@gmail.com>
2022-07-08 12:54:13 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from machine import Pin, PWM
 from time import sleep
-import sys
-import socket
 import network
+import socket
+import sys
 
 
+# create Access Point on ESP32
 wlan = network.WLAN(network.AP_IF)
 wlan.active(True)
 wlan.config(essid="ESP-WIFI")
 
+#create server socket for Wi-Fi network of ESP32
 host = '192.168.4.1'
 port = 9900
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind((host, port))
-sock.listen(5)
+sock.listen(10)
 
 
 # CONSTANTS
 KEY_UP = const(0)
 KEY_DOWN = const(1)
 
+# keys on keyboard
 keys = [['1', '2', '3', 'A'],
         ['4', '5', '6', 'B'],
         ['7', '8', '9', 'C'],
@@ -72,6 +74,7 @@ buzzer = PWM(p23)
 buzzer.duty(0)
 
 
+# send message to the client via Wi-Fi connection
 def send_message(mes):
     client.send(mes.encode('utf-8'))
 
@@ -98,7 +101,8 @@ def scan(row, col):
     # return the key state
     return key
 
-# Function for playing sound
+
+# function for playing sound
 def play(pin, melodies, delays, duty):
 
     pwm = PWM(pin)
@@ -110,8 +114,10 @@ def play(pin, melodies, delays, duty):
     pwm.duty(0)
     pwm.deinit()
 
+        
 # function for setting melody for symbols
 def set_melody(sym):
+        
     if sym == 'A':
         return [
             E7, E7, 0, E7, 0, C7, E7, 0,
@@ -124,7 +130,8 @@ def set_melody(sym):
             0, A6, 0, B6, 0, AS6, A6, 0,
             G6, E7, 0, G7, A7, 0, F7, G7,
             0, E7, 0, C7, D7, B6, 0, 0,
-            ]
+        ]
+
     if sym == 'B':
         return [
             E7, E7, E7, 0,
@@ -136,6 +143,7 @@ def set_melody(sym):
             E7, G7, C7, D7, E7, 0,
             F7, F7, F7, F7, F7, E7, E7, E7, G7, G7, F7, D7, C7, 0
         ]
+
     if sym == 'C':
         return [
             C6, C6, G6, G6, A6, A6, G6, 0,
@@ -144,17 +152,21 @@ def set_melody(sym):
             G6, G6, F6, F6, E6, E6, D6, 0,
             C6, C6, G6, G6, A6, A6, G6, 0,
             F6, F6, E6, E6, D6, D6, C6, 0,
-            ]
+        ]
+
     if sym == 'D':
         return [
-                1915, 0, 1700, 0,
-                1519, 0, 1432, 0,
-                1275, 0, 1136, 0,
-                1014, 0, 956, 0
-                ]
+            1915, 0, 1700, 0,
+            1519, 0, 1432, 0,
+            1275, 0, 1136, 0,
+            1014, 0, 956, 0
+        ]
 
 
+# program launch
 print("Welcome to our Wonderful Piano!")
+
+# accept client connection
 client, address = sock.accept()
 print("You can start to play!")
 print(f"Client with address {address} is waiting for you!")
@@ -168,12 +180,15 @@ try:
         for row in range(4):
             for col in range(4):
                 key = scan(row, col)
-                if key == KEY_DOWN:
+                
+                if key == KEY_DOWN:    
                     cur_key = keys[row][col]
+                
                     if cur_key == "0":
                         send_message("Close session")
                         client.close()
                         sys.exit()
+                        
                     elif cur_key in "12345678":
                         buzzer.freq(tones[int(cur_key) - 1])
                         buzzer.duty(50)
@@ -181,6 +196,7 @@ try:
                         buzzer.duty(0)
                         send_message(f"Key: {notes[int(cur_key) - 1]}")
                         print(f"Key: {notes[int(cur_key) - 1]}")
+                        
                     elif cur_key in "ABCD":
                         send_message(f"Song {cur_key} is playing. Enjoy)")
                         print(f"Song {cur_key} is playing. Enjoy)")
